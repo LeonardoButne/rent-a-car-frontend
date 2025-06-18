3c2323ead320813d2e797713e6643b053790038d
Leonardo <leonardobutne78@gmail.com>
2025-06-18 21:28:12 +0200

    feat: add initial structure

---
+++ rent_a_car_app/lib/core/services/api_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio _dio = Dio(BaseOptions(
+    baseUrl: 'http://localhost:4002',
+    connectTimeout: const Duration(seconds: 5),
+    receiveTimeout: const Duration(seconds: 5),
+  ));
+
+  Future<Response> get(String path) async {
+    return await _dio.get(path);
+  }
+
+  Future<Response> post(String path, dynamic data) async {
+    return await _dio.post(path, data: data);
+  }
+}
